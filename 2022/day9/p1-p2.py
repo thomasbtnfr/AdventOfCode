d85c2d94dd62b6046dede25fcfa8628d1f33dac8
tbetton <thomas.betton@insa-rennes.fr>
2022-12-09 22:33:19 +0100

    day 9 solution

--- 2022/day9/p1-p2.py
+++ 2022/day9/p1-p2.py
@@ -9,7 +9,7 @@ def move(head_position, direction):
 def follow(tail, head):
     tail_x, tail_y = tail[0], tail[1]
     head_x, head_y = head[0], head[1]
-    
+
     if abs(head_x - tail_x) > 1 or abs(head_y - tail_y) > 1:
         if tail_x == head_x:
             tail_y += 1 if head_y > tail_y else -1
@@ -22,7 +22,7 @@ def follow(tail, head):
 
 
 
-
+# Part 1
 head = [0, 0]
 tail = tuple([0, 0])
 tail_visited = set()
@@ -32,7 +32,6 @@ with open('data.txt', 'r') as f:
 
     for m in movements:
         direction, nstep = m.split()
-        print(direction, nstep)
 
         for _ in range(int(nstep)):
             head = move(head, direction)
@@ -41,3 +40,25 @@ with open('data.txt', 'r') as f:
             tail_visited.add(tail)
 
 print(len(tail_visited))
+
+# Part 2
+head = [0, 0]
+tails = [[0, 0], [0,0], [0, 0], [0, 0], [0, 0], [0,0], [0, 0], [0, 0], [0, 0]]
+tail_9_visited = set()
+
+with open('data.txt', 'r') as f:
+    movements = f.read().splitlines()
+
+    for m in movements:
+        direction, nstep = m.split()
+
+        for _ in range(int(nstep)):
+            head = move(head, direction)
+
+            tails[0] = follow(tails[0], head)
+            for i in range(1, len(tails)):
+                tails[i] = follow(tails[i], tails[i-1])
+
+            tail_9_visited.add(tuple(tails[8]))
+
+print(len(tail_9_visited))
